62eadfff73279a6e52fb97c1894b40700ae0d680
Rubin Njagi <rubinnjagi@yahoo.com>
2017-10-19 14:40:15 +0300

    no changes

--- myfront/js/controllers/main.controller.js
+++ myfront/js/controllers/main.controller.js
@@ -85,7 +85,7 @@
 
                  } 
                  else{
-                  $scope.venue.push({"location":"A"})
+                  $scope.venue.push({"location":"A", "class": "away"})
                   $scope.against.push({"team" : $scope.fixtures[i].homeTeamName} )
                   $scope.class="location away";
                  
